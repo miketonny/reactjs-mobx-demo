67baba117629cfe25febcf3b8523e173945a6cca
miketonny <miketonny@gmail.com>
2018-07-12 14:58:42 +1200

    add api urls

--- src/models/Transport.js
+++ src/models/Transport.js
@@ -5,7 +5,10 @@ const _periodApi = '/athletes/periods';
 /**
  * handles data transport layer functions ==============================================
  */
-
+//https://vxapi.azurewebsites.net
+//get api/sessions/{sessionId?}
+//get bm api/benchmarkgroups/{sessionId?}
+//get metrics api/livemetrics/{sessionId?}
 
 export function fetchAthletes(sessionID){
     try { 
